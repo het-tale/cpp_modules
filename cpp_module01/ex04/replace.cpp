b73249b5e735b977e2e92257eae0ec61dde5a715
het-tale <ettalebyhasnaa99@gmail.com>
2022-11-17 05:52:15 +0100

    fix ex04

--- cpp_module01/ex04/replace.cpp
+++ cpp_module01/ex04/replace.cpp
@@ -6,7 +6,7 @@
 /*   By: het-tale <het-tale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 23:58:27 by het-tale          #+#    #+#             */
-/*   Updated: 2022/11/17 00:03:48 by het-tale         ###   ########.fr       */
+/*   Updated: 2022/11/17 05:50:33 by het-tale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ void	replace(char *argv[])
 	std::string		replace;
 	std::ifstream file;
 	std::ofstream replace_file;
-	size_t			pos;
+	size_t			pos = 0;
+	size_t			pp = 0;
 	
 	file.open(argv[1], std::ios::in);
 	if (!file.is_open())
@@ -28,26 +29,27 @@ void	replace(char *argv[])
 		std::cout << argv[1] << " doesn't exist or doesn't have the right permissions\n";
 		return ;
 	}
-	getline(file, str);
-	if (str.empty())
-	{
-		std::cout << argv[1] << " is empty\n";
-		return ;
-	}
 	out_file = argv[1];
 	replace_file.open(out_file.append(".replace"), std::ios::out);
 	search = argv[2];
 	replace = argv[3];
-	while (!str.empty())
+	while (getline(file, str))
 	{
-		pos = str.find(search);
-		if (pos != std::string::npos)
+		if (str.empty())
 		{
+			std::cout << argv[1] << " is empty\n";
+			break ;
+		}
+		while (pos != std::string::npos)
+		{
+			pos = str.find(search, pp);
+			if (pos == std::string::npos)
+				break ;
 			str.erase(pos, search.size());
 			str.insert(pos, replace);
-			replace_file << str << "\n";
+			pp = pp + search.size();
 		}
-		getline(file, str);
+		replace_file << str << "\n";
 	}
 	file.close();
 	replace_file.close();
