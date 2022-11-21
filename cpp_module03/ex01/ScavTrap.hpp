88c28297239095e5acfef2c6097d6beb265f321a
het-tale <ettalebyhasnaa99@gmail.com>
2022-11-21 10:34:29 +0100

    ex01

---
+++ cpp_module03/ex01/ScavTrap.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: het-tale <het-tale@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 10:32:35 by het-tale          #+#    #+#             */
+/*   Updated: 2022/11/21 10:33:55 by het-tale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ScavTrap: public ClapTrap
+{
+    
+};
+#endif
