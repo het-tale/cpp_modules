f60ae7529d3c781b8dc3c4cfbbe8cf02c5491b37
het-tale <ettalebyhasnaa99@gmail.com>
2022-12-06 01:43:42 +0100

    ex00 check

--- cpp_module06/ex00/Static.cpp
+++ cpp_module06/ex00/Static.cpp
@@ -6,7 +6,7 @@
 /*   By: het-tale <het-tale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 10:33:51 by het-tale          #+#    #+#             */
-/*   Updated: 2022/12/05 12:23:52 by het-tale         ###   ########.fr       */
+/*   Updated: 2022/12/06 01:37:52 by het-tale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,10 @@ bool    isInt(char *str)
     return (true);
 }
 
-bool isFloat(char *str)
+bool isFloat(char *str, int *d)
 {
     std::string inff[3] = {"-inff", "+inff", "nanf"};
-    int i = 0, d = 0;
+    int i = 0;
 
     while (i < 3)
     {
@@ -47,28 +47,32 @@ bool isFloat(char *str)
         if (!isdigit(str[i]))
             break ;
         i++;
-        d++;
+        (*d)++;
     }
-    if(str[i] != '.' || d == 0)
+    if(str[i] != '.' || *d == 0)
         return (false);
     i++;
-    d = 0;
+    *d = 0;
     while (str[i])
     {
         if (!isdigit(str[i]))
             break ;
         i++;
-        d++;
+        (*d)++;
     }
-    if (str[i] == 'f' && str[i + 1] == '\0' && d > 0)
+    if (str[i] == 'f' && str[i + 1] == '\0' && *d > 0)
+    {
+        if (str[i - 1] == '0')
+            *d = -1;
         return (true);
+    }
     return (false);
 }
 
-bool isDouble(char *str)
+bool isDouble(char *str, int *d)
 {
     std::string inf[3] = {"-inf", "+inf", "nan"};
-    int i = 0, d = 0;
+    int i = 0;
 
     while (i < 3)
     {
@@ -84,74 +88,89 @@ bool isDouble(char *str)
         if (!isdigit(str[i]))
             break ;
         i++;
-        d++;
+        (*d)++;
     }
-    if(str[i] != '.' || d == 0)
+    if(str[i] != '.' || *d == 0)
         return (false);
     i++;
-    d = 0;
+    *d = 0;
     while (str[i])
     {
         if (!isdigit(str[i]))
             break ;
         i++;
-        d++;
+        (*d)++;
     }
-    if (str[i] == '\0' && d > 0)
+    if (str[i] == '\0' && *d > 0)
+    {
+        if (str[i - 1] == '0')
+            *d = -1;
         return (true);
+    }
     return (false);
 }
 
 void    Convert_all(char *str)
 {
-    int inta;
-    float floata;
-    double doubla;
-    char chara;
+    int inta = 0;
+    float floata = 0;
+    double doubla = 0;
+    char chara = '0';
+    int d = 0;
 
     if (isInt(str))
     {
-        inta = std::stoi(str);
+        d = -1;
+        inta = atoi(str);
         floata = static_cast<float>(inta);
         doubla = static_cast<double>(inta);
         chara = static_cast<char>(inta);
     }
-    else if (isFloat(str))
+    else if (isFloat(str, &d))
     {
-        floata = std::stof(str);
+        floata = strtof(str, NULL);
         inta = static_cast<int>(floata);
         doubla = static_cast<double>(floata);
         chara = static_cast<char>(floata);
     }
-    else if (isDouble(str))
+    else if (isDouble(str, &d))
     {
-        doubla = std::stod(str);
+        doubla = strtod(str, NULL);
         floata = static_cast<float>(doubla);
         inta = static_cast<int>(doubla);
         chara = static_cast<char>(doubla);
     }
     else if (strlen(str) == 1)
     {
+        d = -1;
         chara = str[0];
         floata = static_cast<float>(chara);
         inta = static_cast<int>(chara);
         doubla = static_cast<double>(chara);
     }
-    print_messages(inta, chara, floata, doubla);
+    print_messages(inta, chara, floata, doubla, d);
 }
 
-void    print_messages(int inta, char chara, float floata, double doubla)
+void    print_messages(int inta, char chara, float floata, double doubla, int d)
 {
-    if (!isprint(chara))
-        std::cout << "char: Non displayable" << std::endl;
-    else if (inta > CHAR_MAX || inta < CHAR_MIN)
+    if (inta > CHAR_MAX || inta < CHAR_MIN)
         std::cout << "char: Impossible" << std::endl;
+    else if (!isprint(chara))
+        std::cout << "char: Non displayable" << std::endl;
     else
-        std::cout << "char: " << chara << std::endl;
-    if (inta > INT_MAX || inta < INT_MIN)
+        std::cout << "char: '" << chara << "'" << std::endl;
+    if (inta >= INT_MAX || inta <= INT_MIN)
         std::cout << "int: Impossible" << std::endl;
     else
         std::cout << "int: " << inta << std::endl;
-    std::cout << "float: " << floata << "f" << std::endl;
-    std::cout << "double: " << doubla << std::endl;
+    if (d == -1)
+    {
+        std::cout << "float: " << floata << ".0f" << std::endl;
+        std::cout << "double: " << doubla << ".0" << std::endl;
+    }
+    else
+    {
+        std::cout << "float: " << floata << "f" << std::endl;
+        std::cout << "double: " << doubla << std::endl;
+    }
 }
--- cpp_module06/ex00/Static.hpp
+++ cpp_module06/ex00/Static.hpp
@@ -6,7 +6,7 @@
 /*   By: het-tale <het-tale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 09:56:31 by het-tale          #+#    #+#             */
-/*   Updated: 2022/12/05 12:24:41 by het-tale         ###   ########.fr       */
+/*   Updated: 2022/12/06 01:43:00 by het-tale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,9 @@
 #include <typeinfo>
 
 
-//checkers
 bool    isInt(char *str);
 bool isFloat(char *str);
 bool isDouble(char *str);
 void    Convert_all(char *str);
-void    print_messages(int inta, char chara, float floata, double doubla);
+void    print_messages(int inta, char chara, float floata, double doubla, int d);
 #endif
